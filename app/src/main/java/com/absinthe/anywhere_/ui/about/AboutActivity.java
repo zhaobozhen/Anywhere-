bc653d449015fb4fe4e5edf6c96ea312c3348f30
Bozhen Zhao <zhaobozhen2025@gmail.com>
2019-09-25 20:10:33 +0800

    Support Firebase and Crashlytics

--- app/src/main/java/com/absinthe/anywhere_/ui/about/AboutActivity.java
+++ app/src/main/java/com/absinthe/anywhere_/ui/about/AboutActivity.java
@@ -3,8 +3,12 @@ package com.absinthe.anywhere_.ui.about;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.absinthe.anywhere_.R;
+import com.crashlytics.android.Crashlytics;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -12,5 +16,17 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        Button crashButton = new Button(this);
+        crashButton.setText("Crash!");
+        crashButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Crashlytics.getInstance().crash(); // Force a crash
+            }
+        });
+
+        addContentView(crashButton, new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
     }
 }
