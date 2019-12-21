14b6586e1e58318760b448a1f60e995ee57eda0a
zhaobozhen <zhaobozhen2025@gmail.com>
2019-12-21 17:02:39 +0800

    Update OnceTag.java

--- app/src/main/java/com/absinthe/anywhere_/model/OnceTag.java
+++ app/src/main/java/com/absinthe/anywhere_/model/OnceTag.java
@@ -3,4 +3,6 @@ package com.absinthe.anywhere_.model;
 public class OnceTag {
     public static final String FAB_GUIDE = "fabGuide";
     public static final String QR_COLLECTION_TIP = "qrCollectionTip";
+    public static final String OVERLAY_TIP = "overlayTip";
+
 }
