2bd693c0ab9c78f799f9be68b5effa108b9db0c5
zhaobozhen <zhaobozhen2025@gmail.com>
2020-05-01 10:14:47 +0800

    Update BaseCardAdapter.kt

--- app/src/main/java/com/absinthe/anywhere_/adapter/card/BaseCardAdapter.kt
+++ app/src/main/java/com/absinthe/anywhere_/adapter/card/BaseCardAdapter.kt
@@ -5,16 +5,16 @@ import com.chad.library.adapter.base.BaseDelegateMultiAdapter
 import com.chad.library.adapter.base.delegate.BaseMultiTypeDelegate
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 
-class BaseCardAdapter() : BaseDelegateMultiAdapter<AnywhereEntity, BaseViewHolder> {
+class BaseCardAdapter() : BaseDelegateMultiAdapter<AnywhereEntity, BaseViewHolder>() {
 
-    constructor(var type: Int) : super() {
-        setMultiTypeDelegate(object : BaseMultiTypeDelegate<AnywhereEntity>() {
-            override fun getItemType(data: List<AnywhereEntity>, position: Int): Int {
-                TODO("Not yet implemented")
-            }
-
-        })
-    }
+//    constructor(var type: Int) : super() {
+//        setMultiTypeDelegate(object : BaseMultiTypeDelegate<AnywhereEntity>() {
+//            override fun getItemType(data: List<AnywhereEntity>, position: Int): Int {
+//                TODO("Not yet implemented")
+//            }
+//
+//        })
+//    }
 
     override fun convert(holder: BaseViewHolder, item: AnywhereEntity) {
 
