e6fee560589d4527dcc274fcaa9b704962fabc1b
Absinthe <zhaobozhen2025@gmail.com>
2022-07-16 12:43:29 +0800

    Update deps.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ buildscript {
   }
   dependencies {
     classpath("com.android.tools.build:gradle:7.2.1")
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
-    classpath("com.google.devtools.ksp:symbol-processing-gradle-plugin:1.6.21-1.0.5")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+    classpath("com.google.devtools.ksp:symbol-processing-gradle-plugin:1.7.10-1.0.6")
     classpath("com.github.iwhys:sdk-editor-plugin:1.1.7")
     classpath("com.github.LianjiaTech:gson-plugin:2.1.0")
     classpath("dev.rikka.tools.materialthemebuilder:gradle-plugin:1.3.2")
